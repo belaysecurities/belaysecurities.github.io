78398df9efc3f2cc633b15147a16383d2590266c
Zi Gao <zi.s.gao@gmail.com>
2021-12-11 22:05:18 -0500

    update copy, add spanish

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "0066b98af4c2f781f8644630e5a540c4",
+  "main.dart.js": "6e575b5f6e37aad55a77b19eaa057f2a",
 "manifest.json": "f5688e0ea966e1611fdc70472c946dc5",
 "assets/google_fonts/Raleway-Italic.ttf": "f73026bcd64e5a5265ab616e5083cd48",
 "assets/google_fonts/OpenSans-ExtraBold.ttf": "fb7e3a294cb07a54605a8bb27f0cd528",
@@ -67,8 +67,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "9579be50a1643b4a861ad75236629ddb",
 "assets/NOTICES": "8721b6d775787d834aab4b8128b0ac92",
-"index.html": "af62883feb3f5e05f2bc14f1f1a98e8f",
-"/": "af62883feb3f5e05f2bc14f1f1a98e8f",
+"index.html": "582847c2f628e550eaa4a4b55eb25de5",
+"/": "582847c2f628e550eaa4a4b55eb25de5",
 "version.json": "c84c11bc65b61fea8ca067f67ddc8059",
 "icons/logo.png": "9f286930c0cbdf18d5cfc45fcc8a54ae",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
