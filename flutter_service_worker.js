7872291d6d74617f9e64e86ab50a66b923c3ecfb
Zi Gao <zi.s.gao@gmail.com>
2021-12-13 21:29:03 -0500

    uber sso fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "92ae6056a050017b5b3567f73f8055b7",
+  "main.dart.js": "14459032de9eb0b8b719b14b741e9d52",
 "manifest.json": "f5688e0ea966e1611fdc70472c946dc5",
 "assets/google_fonts/Raleway-Italic.ttf": "f73026bcd64e5a5265ab616e5083cd48",
 "assets/google_fonts/OpenSans-ExtraBold.ttf": "fb7e3a294cb07a54605a8bb27f0cd528",
@@ -67,8 +67,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "9579be50a1643b4a861ad75236629ddb",
 "assets/NOTICES": "8721b6d775787d834aab4b8128b0ac92",
-"index.html": "9f03fcffaafa6cf072e036eb30f3bb5d",
-"/": "9f03fcffaafa6cf072e036eb30f3bb5d",
+"index.html": "c2dcad7f31be2767d0ac9e4e3ae49e80",
+"/": "c2dcad7f31be2767d0ac9e4e3ae49e80",
 "version.json": "c84c11bc65b61fea8ca067f67ddc8059",
 "icons/logo.png": "9f286930c0cbdf18d5cfc45fcc8a54ae",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
